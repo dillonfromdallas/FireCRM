91430a86e43ae3f325cc6d7b6274aeb2072da4d2
dillonreyna <thedillonreyna@gmail.com>
2019-01-01 22:54:37 -0600

    Clients layout complete

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,12 +1,71 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Clients extends Component {
   render() {
-    return (
-      <div>
-        <h1>Clients</h1>
-      </div>
-    );
+    const clients = [
+      {
+        id: "1",
+        firstName: "Kevin",
+        lastName: "Malone",
+        email: "kevin@dundermifflin.com",
+        phone: "867-5309",
+        balance: "100"
+      },
+      {
+        id: "2",
+        firstName: "Creed",
+        lastName: "Bratton",
+        email: "creed@altavista.com",
+        phone: "469-000-0000",
+        balance: "60,000"
+      }
+    ];
+
+    if (clients) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2>
+                <i className="fas fa-users" /> Clients
+              </h2>
+            </div>
+          </div>
+          <table className="table table-striped">
+            <thead className="thead-inverse">
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Balance</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map(client => (
+                <tr key={client.id}>
+                  <td>
+                    {client.firstName} {client.lastName}
+                  </td>
+                  <td>{client.email}</td>
+                  <td>${parseFloat(client.balance).toFixed(2)}</td>
+                  <td>
+                    <Link
+                      to={`/client/${client.id}`}
+                      className="btn btn-secondary btn-sm"
+                    >
+                      <i className="fas fa-arrow-circle-right" /> Details
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    } else {
+      return <h1>Loading...</h1>;
+    }
   }
 }
 
